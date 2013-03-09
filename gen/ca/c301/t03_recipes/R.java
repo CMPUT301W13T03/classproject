a061863d407513a7341029e208c05c17788809d0
gmeyer1 <gmeyer1@ualberta.ca>
2013-03-09 13:23:07 -0700

    Converter class added

--- gen/ca/c301/t03_recipes/R.java
+++ gen/ca/c301/t03_recipes/R.java
@@ -41,24 +41,22 @@ public final class R {
         public static final int textView_instructions=0x7f070017;
     }
     public static final class layout {
-        public static final int activity_add_ingredient=0x7f030000;
-        public static final int activity_add_recipe=0x7f030001;
-        public static final int activity_edit_recipe=0x7f030002;
-        public static final int activity_ingredient_list=0x7f030003;
-        public static final int activity_main=0x7f030004;
-        public static final int activity_photo=0x7f030005;
-        public static final int activity_search=0x7f030006;
-        public static final int activity_view_recipe=0x7f030007;
+        public static final int activity_add_recipe=0x7f030000;
+        public static final int activity_edit_recipe=0x7f030001;
+        public static final int activity_ingredient_list=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_photo=0x7f030004;
+        public static final int activity_search=0x7f030005;
+        public static final int activity_view_recipe=0x7f030006;
     }
     public static final class menu {
-        public static final int activity_add_ingredient=0x7f060000;
-        public static final int activity_add_recipe=0x7f060001;
-        public static final int activity_edit_recipe=0x7f060002;
-        public static final int activity_ingredient_list=0x7f060003;
-        public static final int activity_main=0x7f060004;
-        public static final int activity_photo=0x7f060005;
-        public static final int activity_search=0x7f060006;
-        public static final int activity_view_recipe=0x7f060007;
+        public static final int activity_add_recipe=0x7f060000;
+        public static final int activity_edit_recipe=0x7f060001;
+        public static final int activity_ingredient_list=0x7f060002;
+        public static final int activity_main=0x7f060003;
+        public static final int activity_photo=0x7f060004;
+        public static final int activity_search=0x7f060005;
+        public static final int activity_view_recipe=0x7f060006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
