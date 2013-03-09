e783b1fe5d5da30bb01991d6f89ab3022c6ba6f3
gmeyer1 <gmeyer1@ualberta.ca>
2013-03-09 14:53:56 -0700

    Added name entry

--- src/ca/c301/t03_recipes/AddRecipeActivity.java
+++ src/ca/c301/t03_recipes/AddRecipeActivity.java
@@ -1,5 +1,6 @@
 package ca.c301.t03_recipes;
 
+import ca.c301.t03_model.Recipe;
 import ca.c301.t03_recipes.R;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddRecipeActivity extends Activity {
  
@@ -16,16 +18,27 @@ public class AddRecipeActivity extends Activity {
 	 * @uml.associationEnd inverse="addRecipeActivity:ca.c301.t03_recipes.RecipeApplication"
 	 */
 	private RecipeApplication recipeApplication;
+	private Recipe recipe;
+	
+	private EditText name;
+	private EditText instructions;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_recipe);
 		
+		recipe = new Recipe();
+		
+		name = (EditText) findViewById(R.id.editText_name);
+		instructions = (EditText) findViewById(R.id.editText_instructions);
+		
 		Button saveButton = (Button) findViewById(R.id.button_save);
         saveButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
+            	recipe.setName(name.getText().toString());
+            	recipe.setInstructions(instructions.getText().toString());
             	finish();
             }
         });
