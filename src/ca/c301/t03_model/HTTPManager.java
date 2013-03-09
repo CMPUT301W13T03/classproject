2de4f00e5a6ea93e88b3ef259a68156bcab67fd0
aruffell <aruffell@ualberta.ca>
2013-03-08 18:09:38 -0700

    Started implementation of addRecipe, it's probably broken.

--- src/ca/c301/t03_model/HTTPManager.java
+++ src/ca/c301/t03_model/HTTPManager.java
@@ -1,5 +1,73 @@
 package ca.c301.t03_model;
 
-public class HTTPManager {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+
+public class HTTPManager {
+	// Http Connector
+	private HttpClient httpclient = new DefaultHttpClient();
+
+	// JSON Utilities
+	private Gson gson = new Gson();
+
+	/**
+	 * Consumes the POST/Insert operation of the service
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 */
+	public void addRecipe(Recipe recipe) throws IllegalStateException, IOException{
+		// TODO httpPost is currently directed at the testing server, will need to change to "http://cmput301.softwareprocess.es:8080/CMPUT301W13T03/"
+		HttpPost httpPost = new HttpPost("http://cmput301.softwareprocess.es:8080/testing/"+recipe.getId());
+		StringEntity stringentity = null;
+		try {
+			stringentity = new StringEntity(gson.toJson(recipe));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		httpPost.setHeader("Accept","application/json");
+
+		httpPost.setEntity(stringentity);
+		HttpResponse response = null;
+		try {
+			response = httpclient.execute(httpPost);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String status = response.getStatusLine().toString();
+		System.out.println(status);
+		HttpEntity entity = response.getEntity();
+		BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
+		String output;
+		System.err.println("Output from Server -> ");
+		while ((output = br.readLine()) != null) {
+			System.err.println(output);
+		}
+
+		try {
+			// May need if statement, check isStreaming();
+			entity.consumeContent();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//May possibly need to deallocate more resources here. No 4.0 implementation of releaseconnection();
+	}
 }
