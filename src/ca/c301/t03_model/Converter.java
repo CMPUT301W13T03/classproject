7848c2adf092cf46ed6371bbc172b984d220e3fb
gmeyer1 <gmeyer1@ualberta.ca>
2013-03-09 19:16:06 -0700

    Missing classes added (hopefully)

--- src/ca/c301/t03_model/Converter.java
+++ src/ca/c301/t03_model/Converter.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 public class Converter {
 
 	public String[] convertList(ArrayList<Ingredient> ingredients) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		
+		for (int i = 0; i < ingredients.size(); i++) {
+			stringList.add(ingredients.get(i).getName() + "\n" + String.valueOf(ingredients.get(i).getAmount()) + " " + ingredients.get(i).getUnitOfMeasurement());
+		}
+		
+		String[] outArray = new String[stringList.size()];
+		stringList.toArray(outArray);
+		
+		return outArray;
 	}
-
 }
--- src/ca/c301/t03_recipes/EditIngredientActivity.java
+++ src/ca/c301/t03_recipes/EditIngredientActivity.java
@@ -1,5 +1,84 @@
 package ca.c301.t03_recipes;
 
-public class EditIngredientActivity {
+import ca.c301.t03_model.Ingredient;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+ * TODO:
+ * Add tests for proper input
+ */
+public class EditIngredientActivity extends Activity {
+
+private Ingredient ingredient;
+	
+	private EditText name;
+	private EditText amount;
+	private EditText unit;
+	private int index;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_ingredient);
+		
+		Bundle data = getIntent().getExtras();
+		index = data.getInt("index");
+		
+		ingredient = new Ingredient();
+		
+		name = (EditText) findViewById(R.id.editText_ingredient);
+		amount = (EditText) findViewById(R.id.editText_amount);
+		unit = (EditText) findViewById(R.id.editText_unit_type);
+		
+		name.setText(data.getString("name"));
+		amount.setText(String.valueOf(data.getDouble("amount")));
+		unit.setText(data.getString("unit"));
+		
+		Button saveButton = (Button) findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+            	
+            	Intent returnIntent = new Intent();
+            	returnIntent.putExtra("name",name.getText().toString());
+            	returnIntent.putExtra("amount",Double.valueOf(amount.getText().toString()));
+            	returnIntent.putExtra("unit",unit.getText().toString());
+            	returnIntent.putExtra("type",1);
+            	returnIntent.putExtra("index", index);
+            	returnIntent.putExtra("del",0);
+            	setResult(RESULT_OK,returnIntent); 
+            	
+            	finish();
+            }
+        });
+        
+        Button deleteButton = (Button) findViewById(R.id.button_delete);
+        deleteButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+            	
+            	Intent returnIntent = new Intent();
+            	returnIntent.putExtra("del",1);
+            	returnIntent.putExtra("index", index);
+            	setResult(RESULT_OK,returnIntent); 
+            	
+            	finish();
+            }
+        });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_edit_ingredient, menu);
+		return true;
+	}
 
 }
