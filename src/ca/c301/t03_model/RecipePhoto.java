b1e01964cb85b52811d7e6efee9d79fff20ada53
stankiev <stankiev@uc04.cs.ualberta.ca>
2013-04-02 13:23:17 -0600

    RecipePhoto had hidden from being Javadoc'd!

--- src/ca/c301/t03_model/RecipePhoto.java
+++ src/ca/c301/t03_model/RecipePhoto.java
@@ -7,23 +7,51 @@ import java.net.URI;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 
-/*
- * Handles storage of photos for recipes - NOT IMPLEMENTED
+/**
+ * Handles storage of photos for recipes
  */
 public class RecipePhoto implements Serializable{
 
 	File photoFile;
+	
+	/**
+	 * Constructor given a file containing the photo
+	 * 
+	 * @param f
+	 * 				Is the file containing the photo
+	 */
 	public RecipePhoto(File f) {
 		photoFile = f;
 	}
 
 	private static final long serialVersionUID = -2415081243980986524L;
+	
+	/**
+	 * Getter of the file containing the photo
+	 * 
+	 * @return
+	 * 				The file containing the photo
+	 */
 	public File getFile(){
 		return this.photoFile;
 	}
+	
+	/**
+	 * Getter of the URI of the photo file
+	 * 
+	 * @return
+	 * 				The URI for the file containing the photo
+	 */
 	public Uri getURI() {
 		return Uri.fromFile(photoFile);
 	}
+	
+	/**
+	 * Gets a Drawable object from the photo file
+	 * 
+	 * @return
+	 * 				A Drawable object of the photo file
+	 */
 	public Drawable getDrawable() {
 		return Drawable.createFromPath(getURI().getPath());
 	}
