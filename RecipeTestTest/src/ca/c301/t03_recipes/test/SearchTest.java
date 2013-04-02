c682c61bea402abb04a3cf10bc1a65575001029e
gmeyer1 <gmeyer1@ualberta.ca>
2013-04-02 11:46:55 -0600

    added search testing class

---
+++ RecipeTestTest/src/ca/c301/t03_recipes/test/SearchTest.java
@@ -0,0 +1,13 @@
+package ca.c301.t03_recipes.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ca.c301.t03_recipes.MainActivity;
+
+public class SearchTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+	public SearchTest() {
+		super(MainActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
